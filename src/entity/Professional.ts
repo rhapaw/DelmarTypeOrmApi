175952fb3d3b31c4630dd0e281e06178330a453e
Kevin Mitchell <kevin475@hotmail.com>
2019-11-19 04:00:03 -0800

    Initial2

---
+++ src/entity/Professional.ts
@@ -0,0 +1,13 @@
+export class Professional {
+    id: number;
+    firstName: string;
+    lastName: string;
+    photoUrl: string;
+    phoneOffice: string;
+    phoneMobile: string;
+    email: string;
+    department: string;
+    position: string;
+    specialization: string;
+    blurb: string;
+}
---
+++ src/entity/Property.ts
@@ -0,0 +1,34 @@
+export class Property {
+    id: number;
+    propertyType: string;
+    subType: string;
+    zoning: string;
+    usage: string;
+    buildingClass: string;
+    saleType: string;
+    parcelId: string;
+    salePrice: number;
+    rentalPrice: number;
+    sizeSf: number;
+    numberOfUnits: number;
+    numberOfFloors: number;
+    typicalFloorSize: number;
+    yearBuilt: number;
+    lotSize: number;
+    lotSizeUnits: string;
+    parkingSpaces: number;
+    parkingPer1000sf: number;
+    description: string;
+    highlights: string;
+    dockingHighDoors: number;
+    buildingSpace: number;
+    buildingSpaceUnits: string;
+    leaseOfficeSizeSf: number;
+    leaseFloor: number;
+    leaseFrom: Date;
+    leaseTo: Date;
+
+
+
+
+}
